db67391db87c592a661ca61983abbf8758165b44
Dario Herrero <dario.herrero@gmail.com>
2019-12-20 11:15:14 -0300

    Update EncryptDecrypt.java

--- Encryption-Decryption/task/src/encryptdecrypt/EncryptDecrypt.java
+++ Encryption-Decryption/task/src/encryptdecrypt/EncryptDecrypt.java
@@ -2,6 +2,4 @@ package encryptdecrypt;
 
 public interface EncryptDecrypt {
     char[] processMessage(String message, int n, String alg);
-
-//    char[] decrypt(String message, int n, String alg);
 }
